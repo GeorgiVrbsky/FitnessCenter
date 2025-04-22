e75893a133c8a8c48f6a9066d539bd7e7ec5265b
GeorgiVrbsky <gogo.vrbsky@gmail.com>
2025-04-22 16:12:14 +0200

    vytvoreni adresaru pro lepsi orientaci

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 session_start();
 
 if(!isset($_SESSION["user_id"])){
-    header("Location: login_page.php");
+    header("Location: /User/login_page.php");
     exit();
 }else{
     echo "Ahoooj " . $_SESSION["jmeno"];
-    header("Location: dashboard_page.php");
+    header("Location: /Dashboard/dashboard_page.php");
 }
 
 ?>
